921214a56e2b8229adbfb8884114ef20698c7788
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-19 01:26:40 +0900

    Initial Commit

---
+++ AsianPokedexApp/AsianPokedexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsianPokedexAppApp.swift
+//  AsianPokedexApp
+//
+//  Created by 渡邊魁優 on 2023/02/19.
+//
+
+import SwiftUI
+
+@main
+struct AsianPokedexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
